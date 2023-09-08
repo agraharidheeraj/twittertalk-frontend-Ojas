87f8db7a89583e7df42814b3a3f9fdc16a8e00b4
dheeraj_700 <dheeraj700agrahari@gmail.com>
2023-09-08 13:47:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter,Routes,Route,Navigate } from 'react-router-dom';
+import Home from './Pages/HomePage/Home';
+import Login from './Pages/LoginPage/login';
+import Profile from './Pages/ProfilePage/profile';
+import {useMemo} from "react";
+import {  useSelector } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material';
+import { themeSettings } from './theme';
+import { Toaster } from 'react-hot-toast';
 
-function App() {
+
+
+const App = () => {
+
+  const mode =  useSelector((state)=> state.mode)
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]); 
+  const isAuth = Boolean(useSelector((state) => state.token));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+     
+  
+  <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Toaster />
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route
+              path="/home"
+              element={isAuth ? <Home /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <Profile /> : <Navigate to="/" />}
+            />
+          </Routes>
+        
+        </ThemeProvider>
+      </BrowserRouter>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
